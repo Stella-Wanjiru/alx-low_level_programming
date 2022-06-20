dfbf51abd0487891b9d436e6cf190bfa73ebddd5
Stella-Wanjiru <stellahallexies@gmail.com>
2022-06-20 10:46:21 -0700

    Memset

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is a readme file containing the C languange.
 Functions and nested loops
+Pointers, arrays and strings readme file
--- main.h
+++ main.h
@@ -1,4 +1,10 @@
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);

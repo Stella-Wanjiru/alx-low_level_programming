b7de89836756a23babe06cfd9f5a103e1e34cc64
Stella-Wanjiru <stellahallexies@gmail.com>
2022-06-20 11:05:36 -0700

    Strpbrk

---
+++ 4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return: a pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}

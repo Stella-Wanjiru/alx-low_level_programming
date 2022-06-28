31e12d93ddac404ea629e354ea5adafa38110c64
Stella-Wanjiru <stellahallexies@gmail.com>
2022-06-28 12:41:50 -0700

    Strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,18 +10,23 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int i, j;
-	char *strout;
+	char *strDup;
+	int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-	if (strout == NUll)
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	strDup = malloc(sizeof(*str) * i);
+	if (strDup == NULL)
 		return (NULL);
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
-
-	return (strout);
+	j=0;
+	while (str[j] != '\0')
+	{
+		strDup[j] = str[j];
+		j++;
+	}
+	return (strDup);
 }

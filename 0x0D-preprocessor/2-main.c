49f16cbc1be0eadb379cd63dadd36f04b450dc14
Stella-Wanjiru <stellahallexies@gmail.com>
2022-07-04 10:53:09 -0700

    Main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file followed by a new line
+ *
+ * Return: Alwats 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
